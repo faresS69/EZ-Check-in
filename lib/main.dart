197fe2ca8630dc3b996c8b1099a3d663c6c5d986
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-11-23 21:40:02 +0100

    add multiprovider + GoogleSheetsProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:ez_check_in/providers/gsheets_provider.dart';
 import 'package:ez_check_in/qr_scanner_screen/init_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'attendees_list/attendees_list.dart';
 import 'attendees_list/attendees_list_from_json.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(
+          create: (_) => GoogleSheetsProvider("assets/credentials.json",
+              "1lzZvXZ3-Sb2vDSLPZf9Rgo5bQtKCoTHu0EUwCdBNeQU", "Sheet1")
+            ..initializeForWorksheet())
+    ], child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,11 +21,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    context.read<GoogleSheetsProvider>();
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
