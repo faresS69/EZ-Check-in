7cca7e1fdcc144a2c0becb26f2e732b55e821f12
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-11-23 21:44:31 +0100

    change network images with local ones

--- lib/qr_scanner_screen/init_screen.dart
+++ lib/qr_scanner_screen/init_screen.dart
@@ -1,5 +1,7 @@
+import 'package:ez_check_in/providers/gsheets_provider.dart';
 import 'package:ez_check_in/qr_scanner_screen/qr_scanner_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/custom_drawer.dart';
 
@@ -17,8 +19,10 @@ class InitQRScanner extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(
-              'https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_500,h_500,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-goog/events/DF23-Bevy-EventThumb-Blue%402x_ecdK6rb.png', // Replace with actual URL
+            // Image.network(
+            // 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_500,h_500,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-goog/events/DF23-Bevy-EventThumb-Blue%402x_ecdK6rb.png', // Replace with actual URL
+            Image.asset(
+              "assets/images/devfestIcon.webp",
               height: 200, // Adjust the size as needed
             ),
             const SizedBox(height: 20),
