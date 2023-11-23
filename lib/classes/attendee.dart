da46279b7096258bdfdfd34946634002d06d2dc9
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-11-23 21:43:38 +0100

    add fromSheets factory methods to attendee class

--- lib/classes/attendee.dart
+++ lib/classes/attendee.dart
@@ -2,18 +2,26 @@ class Attendee {
   final String name;
   final String email;
   final String phoneNumber;
+  final bool checkIn;
 
-  Attendee({
-    required this.name,
-    required this.email,
-    required this.phoneNumber,
-  });
+  Attendee(
+      {required this.name,
+      required this.email,
+      required this.phoneNumber,
+      required this.checkIn});
 
   factory Attendee.fromJson(Map<String, dynamic> json) {
     return Attendee(
-      name: json['name'],
-      email: json['email'],
-      phoneNumber: json['phoneNumber'],
-    );
+        name: json['name'],
+        email: json['email'],
+        phoneNumber: json['phoneNumber'],
+        checkIn: json["checkIn"]);
+  }
+  factory Attendee.fromSheets(List<String> sheets) {
+    return Attendee(
+        name: sheets[0],
+        email: sheets[1],
+        phoneNumber: sheets[2],
+        checkIn: sheets[3].toLowerCase() == "true");
   }
 }
