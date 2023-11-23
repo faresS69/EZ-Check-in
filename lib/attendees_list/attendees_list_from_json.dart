96d7395e153db58c90f2eaf0121d365a0939c2ac
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-11-23 21:41:47 +0100

    use gsheets to fetch attendees data

--- lib/attendees_list/attendees_list_from_json.dart
+++ lib/attendees_list/attendees_list_from_json.dart
@@ -1,71 +1,32 @@
-import '../classes/attendee.dart';
+import 'dart:convert';
 
-List<Attendee> loadAttendeesFromJson() {
-  // Assuming your JSON data is in a variable named jsonData
-  List<dynamic> jsonData = [
-    {
-      "name": "John Doe",
-      "email": "john.doe@example.com",
-      "phoneNumber": "123-456-7890"
-    },
-    {
-      "name": "Jane Smith",
-      "email": "jane.smith@example.com",
-      "phoneNumber": "555-555-5555"
-    },
-    {
-      "name": "Sam Johnson",
-      "email": "sam.johnson@example.com",
-      "phoneNumber": "987-654-3210"
-    },
-    {
-      "name": "Alex Brown",
-      "email": "alex.brown@example.com",
-      "phoneNumber": "111-222-3333"
-    },
-    {
-      "name": "Emily Davis",
-      "email": "emily.davis@example.com",
-      "phoneNumber": "444-444-4444"
-    },
-    {
-      "name": "Michael Lee",
-      "email": "michael.lee@example.com",
-      "phoneNumber": "777-777-7777"
-    },
-    {
-      "name": "Sarah Wilson",
-      "email": "sarah.wilson@example.com",
-      "phoneNumber": "666-666-6666"
-    },
-    {
-      "name": "Ryan Taylor",
-      "email": "ryan.taylor@example.com",
-      "phoneNumber": "999-999-9999"
-    },
-    {
-      "name": "Olivia Brown",
-      "email": "olivia.brown@example.com",
-      "phoneNumber": "888-888-8888"
-    },
-    {
-      "name": "Ethan Davis",
-      "email": "ethan.davis@example.com",
-      "phoneNumber": "222-333-4444"
-    }
-  ]
-  ;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import '../classes/attendee.dart';
+import "package:gsheets/gsheets.dart";
+
+Future<List<Attendee>> loadAttendeesFromJson() async {
+  String credentialsJson =
+      await rootBundle.loadString('assets/credentials.json');
+  var gsheets = GSheets(jsonDecode(credentialsJson));
+  final ss =
+      await gsheets.spreadsheet("1lzZvXZ3-Sb2vDSLPZf9Rgo5bQtKCoTHu0EUwCdBNeQU");
+  final ws = ss.worksheetByTitle("Sheet1");
+  if (ws == null) return List<Attendee>.empty();
+  var gsheetsAttendees = await ws.values.allRows(fromRow: 2);
 
   List<Attendee> attendees = [];
 
-  for (var data in jsonData) {
+  for (var data in gsheetsAttendees) {
     // Assuming your Attendee class has a constructor that takes the necessary data
     Attendee attendee = Attendee(
-      name: data['name'],
-      email: data['email'],
-      phoneNumber: data['phoneNumber'],
-      // Add any other properties as needed
-    );
+        name: data[0],
+        email: data[1],
+        phoneNumber: data[2],
+        checkIn: data[3].toLowerCase() == "true"
+        // Add any other properties as needed
+        );
 
     attendees.add(attendee);
   }
