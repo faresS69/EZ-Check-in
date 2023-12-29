25301150c12bc3fcc1661a34c1a1820b3f2cd2b4
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-12-29 10:27:57 +0100

    make main async

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:ez_check_in/providers/gsheets_provider.dart';
 import 'package:ez_check_in/qr_scanner_screen/init_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(
     MultiProvider(providers: [
-      ChangeNotifierProvider(
-          create: (_) => GoogleSheetsProvider("")..initializeForWorksheet())
+      ChangeNotifierProvider(create: (_) {
+        return GoogleSheetsProvider("")..initializeForWorksheet();
+      })
     ], child: const MyApp()),
   );
 }
@@ -17,6 +21,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    ).then((value) => print(value));
+    // FirebaseAuth auth = FirebaseAuth.instance;
+    // auth
+    //     .signInWithProvider(
+    //       GoogleAuthProvider(),
+    //     )
+    //     .then((value) => print(value));
     context.read<GoogleSheetsProvider>();
     return MaterialApp(
       title: 'Flutter Demo',
